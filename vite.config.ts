6fb7fb48444bb97099493f88bd2735570b06869d
Gokulakrishnan-maker <gokulakrishnan756@gmail.com>
2025-11-16 00:04:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",   // ⭐ IMPORTANT FOR NETLIFY DEPLOYMENT
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
